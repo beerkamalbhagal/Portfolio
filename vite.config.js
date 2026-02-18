519b1de997a11250ad1279bc562c5440f6a5b864
beerkamalbhagal <bbhagal06@gmail.com>
2026-02-18 16:30:31 -0500

    added env variable

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-      react(),
-    tailwindcss(),
-  ],
-})
+let process;
+export default defineConfig(({ command }) => ({
+    plugins: [react(), tailwindcss()],
+    base: process.env.VITE_BASE_PATH || "/Portfolio/",
+}))
